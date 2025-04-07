bd80a7eca84fb217ef164c423dc0f7d11f197310
rissenfire <sullivaec@gmail.com>
2025-04-07 15:49:09 -0400

    sql update

--- submit_fieldnotes.php
+++ submit_fieldnotes.php
@@ -2,18 +2,18 @@
 include 'includes/db.php';
 
 // Get form data
-$tree_id = (int)$_POST['tree_id'];
-$tree_missing = isset($_POST['tree_missing']) ? 1 : 0;
-$sign_missing = isset($_POST['sign_missing']) ? 1 : 0;
-$notes = htmlspecialchars($_POST['notes']);
+$tree_id = (int)$_POST['TREE_ID'];
+$tree_missing = isset($_POST['TREE_MISSING']) ? 1 : 0;
+$sign_missing = isset($_POST['SIGN_MISSING']) ? 1 : 0;
+$notes = htmlspecialchars($_POST['TREE_NOTES']);
 $date_submitted = date('Y-m-d H:i:s');
 
 
 
 // Insert data
 $stmt = $conn->prepare("
-    INSERT INTO field_notes 
-    (tree_id, tree_missing, sign_missing, notes)
+    INSERT INTO FIELD_NOTES 
+    (TREE_ID, TREE_MISSING, SIGN_MISSING, TREE_NOTES)
     VALUES (?, ?, ?, ?)
 ");
 $stmt->bind_param("iiis", $tree_id, $tree_missing, $sign_missing, $notes);
