6200310b23e5d666db9952c01948a85d7f8173e2
rissenfire <sullivaec@gmail.com>
2025-04-07 15:34:56 -0400

    sql update

--- get_tree_metadata.php
+++ get_tree_metadata.php
@@ -3,7 +3,7 @@ header('Content-Type: application/json');
 include 'includes/db.php';
 
 $treeId = (int)$_GET['tree_id'];
-$stmt = $conn->prepare("SELECT scientific_name, PURL FROM trees WHERE tree_id = ?");
+$stmt = $conn->prepare("SELECT SCIENTIFIC_NAME, URL FROM TREES WHERE tTREE_ID = ?");
 $stmt->bind_param("i", $treeId);
 $stmt->execute();
 $result = $stmt->get_result();
