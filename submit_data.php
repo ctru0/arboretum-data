606f6e62cccd1d7dfbc8adc72d00600276cfcd18
rissenfire <sullivaec@gmail.com>
2025-04-07 15:42:06 -0400

    Update submit_data.php

--- submit_data.php
+++ submit_data.php
@@ -2,18 +2,18 @@
 include 'includes/db.php';
 
 
-$tree_id = (int)$_POST['tree_id'];
-$height1 = (float)$_POST['height1'];
-$height2 = (float)$_POST['height2'];
-$height3 = (float)$_POST['height3'];
-$circumference = (float)$_POST['circumference'];
-$student_name = htmlspecialchars(trim($_POST['student_name']));
+$tree_id = (int)$_POST['TREE_ID'];
+$height1 = (float)$_POST['HEIGHT_1'];
+$height2 = (float)$_POST['HEIGHT_2'];
+$height3 = (float)$_POST['HEIGHT_3'];
+$circumference = (float)$_POST['CIRCUMFERENCE'];
+$netid = htmlspecialchars(trim($_POST['NETID']));
 
 $stmt = $conn->prepare("
-    INSERT INTO measurements 
-    (tree_id, height1, height2, height3, circumference, student_name) 
+    INSERT INTO ENTRIES 
+    (TREE_ID, HEIGHT_1, HEIGHT_2, HEIGHT_3, CIRCUMFERENCE, NETID) 
     VALUES (?, ?, ?, ?, ?, ?)");
-$stmt->bind_param("idddds", $tree_id, $height1, $height2, $height3, $circumference, $student_name);
+$stmt->bind_param("idddds", $tree_id, $height1, $height2, $height3, $circumference, $netid);
 
 if ($stmt->execute()) {
     header("Location: view_data.php?success=1");
