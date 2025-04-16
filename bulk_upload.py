81b357571683c7a9da0d970f6a2e2883a679af38
rissenfire <sullivaec@gmail.com>
2025-04-16 12:15:53 -0400

    Update bulk_upload.py

--- bulk_upload.py
+++ bulk_upload.py
@@ -15,21 +15,21 @@ df = pd.read_csv(csv_file)
 df.rename(columns={'PlantSoon URL': 'PlantSoonURL'}, inplace=True)
 
 unique_names = {}
+
 try:
     connection = mysql.connector.connect(**db_config)
     if connection.is_connected():
         cursor = connection.cursor()
         
-        # Check existing names in the database
+        # Retrieve all existing scientific names from the database
         cursor.execute("SELECT scientific_name FROM trees")
         existing_names = set([row[0] for row in cursor.fetchall()])
-        
+
         for index, row in df.iterrows():
             scientific_name = row['Scientific Name'].strip()
-            
-            # Check if the scientific name already exists in the database
+
+            # If the scientific name exists in the database or has been encountered before, increment the suffix
             if scientific_name in existing_names:
-                # Increment the count for this name to prevent duplicates
                 if scientific_name not in unique_names:
                     unique_names[scientific_name] = 1
                 else:
@@ -37,12 +37,13 @@ try:
                 # Append (2), (3), etc.
                 scientific_name = f"{scientific_name} ({unique_names[scientific_name]})"
                 df.at[index, 'Scientific Name'] = scientific_name
-                existing_names.add(scientific_name)  # Add modified name to existing list
             
-            # Prepare data for insertion
+            # Add the modified name to the set of existing names
+            existing_names.add(scientific_name)
+            
             common_name = row['Common Name']
             purl = row['PlantSoonURL']
-            
+
             query = """
             INSERT INTO trees (common_name, scientific_name, PURL)
             VALUES (%s, %s, %s)
