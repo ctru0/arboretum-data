b4fac228bdd22d97d1e092c91d6bc8e0ebceabd1
rissenfire <sullivaec@gmail.com>
2025-04-16 12:19:54 -0400

    Update bulk_upload.py

--- bulk_upload.py
+++ bulk_upload.py
@@ -20,7 +20,7 @@ try:
     connection = mysql.connector.connect(**db_config)
     if connection.is_connected():
         cursor = connection.cursor()
-        
+
         # Retrieve all existing scientific names from the database
         cursor.execute("SELECT scientific_name FROM trees")
         existing_names = set([row[0] for row in cursor.fetchall()])
@@ -28,22 +28,23 @@ try:
         for index, row in df.iterrows():
             scientific_name = row['Scientific Name'].strip()
 
-            # If the scientific name exists in the database or has been encountered before, increment the suffix
+            # Check if the scientific name already exists in the database
             if scientific_name in existing_names:
                 if scientific_name not in unique_names:
                     unique_names[scientific_name] = 1
                 else:
                     unique_names[scientific_name] += 1
-                # Append (2), (3), etc.
+                # Modify the scientific name by appending (2), (3), etc.
                 scientific_name = f"{scientific_name} ({unique_names[scientific_name]})"
                 df.at[index, 'Scientific Name'] = scientific_name
-            
-            # Add the modified name to the set of existing names
+
+            # Add the modified or original name to the set of existing names
             existing_names.add(scientific_name)
             
             common_name = row['Common Name']
             purl = row['PlantSoonURL']
 
+            # Insert or update the entry in the database
             query = """
             INSERT INTO trees (common_name, scientific_name, PURL)
             VALUES (%s, %s, %s)
